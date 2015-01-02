3d5adac5a55a1844d54030fb85d9162f35cddb42
Byron Mallett <byronated@gmail.com>
2015-01-02 17:52:35 +1300

    Removed old job scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,6 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :jobs
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
